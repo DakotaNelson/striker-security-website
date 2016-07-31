59b0ae2aa66d2a67e02b8a48e7195c9b5dcf9e0a
Dakota Nelson <Dakota.Nelson@students.olin.edu>
2016-07-30 21:54:35 -0700

    website mostly done!

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,33 +1,36 @@
 (function($) {
-  // Newsletter signup
-    $('#newsletter .submit').on("click", function() {
-      ga('send', 'event', 'NewsletterSignup', 'Submit');
-
-      // TODO actually send the data to mailchimp
-      var emailAddress = $("#newsletter input[name=email]").val();
-
-      event.preventDefault();
-    });
-
   // Contact form
-    $('#send-message').on("click", function() {
+    var contactForm = $('#contact-form');
+    contactForm.submit(function(e) {
+      e.preventDefault();
+
       ga('send', 'event', 'ContactForm', 'Submit');
 
-      // TODO actually send the data (formspree?)
-      var emailAddress = $("#contact-form input[name=email]").val();
-      var name = $("#contact-form input[name=name]").val();
-      var subject = $("#contact-form input[name=subject]").val();
-      var message = $("#contact-form textarea[name=message]").val();
+      // var emailAddress = $("#contact-form input[name=email]").val();
+      // var name = $("#contact-form input[name=name]").val();
+      // var subject = $("#contact-form input[name=subject]").val();
+      // var message = $("#contact-form textarea[name=message]").val();
 
       // TODO see if validation is necessary
       // if (!emailAddress) {
       //   console.log("no email address given");
       // }
 
-      $("#form-result").text("Thanks! We're excited to follow up with you soon.");
-
-      document.getElementById('contact-form').reset();
-
-      event.preventDefault();
+      $.ajax({
+        url: "//formspree.io/dakota@strikersecurity.com",
+        method: 'POST',
+        data: $(this).serialize(),
+        dataType: 'json',
+        beforeSend: function() {
+          $("#form-result").text("Message sending...");
+        },
+        success: function() {
+          $("#form-result").text("Thanks! We're excited to follow up with you soon.");
+          document.getElementById('contact-form').reset();
+        },
+        error: function() {
+          $("#form-result").text("There was an error sending your message. Please try again or email consulting@strikersecurity.com");
+        }
+      });
     });
 })(jQuery);
